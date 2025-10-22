4ac79f0f84e15032cad0adad0f9f6ac16780f46d
ayoubbadaou <aboubd2000@gmail.com>
2025-10-22 14:21:18 +0200

    Delete Node red storage/Db_pub_read/launch.sh

--- Node red storage/Db_pub_read/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gnome-terminal --tab --title="Data Runner" --command "bash -c 'python3 /home/robot/Bureau/Data_run/data_run.py; exec bash'" \
-               --tab --title="DB Publisher" --command "bash -c 'python3 /home/robot/Bureau/Node-red_storage/Db_pub_read/db_pub.py; exec bash'" \
-               --tab --title="DB Reader" --command "bash -c 'sleep 2; python3 /home/robot/Bureau/Node-red_storage/Db_pub_read/db_read.py; exec bash'"
